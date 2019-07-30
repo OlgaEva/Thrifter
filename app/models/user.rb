8a801b66873546b92d7d7acb8f3715f0c59e13d5
github username <github email address>
2019-07-30 10:57:17 -0400

    add_items_index_and_show_pages

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
     validates :user_name, presence: true
 
     has_many :sellers
-    has_many :tems, through: :sellers
+    has_many :items, through: :sellers
 
     has_many :buyers
     has_many :bought_items, through: :buyers, source: :item
