e631bd7df1ceaa60d1d19d5beecb091fbf605802
github username <github email address>
2019-07-30 16:56:43 -0400

    edit new items form

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
 
-    validates :user_name, presence: true
+    validates :user_name, :password, presence: true
 
     has_many :sellers
     has_many :items, through: :sellers
