af5c828ef42314a24433d328845d37dd457feac7
TeenageGrampa <danielhvoigt@gmail.com>
2019-07-31 12:21:20 -0400

    added transactions

--- app/controllers/buyers_controller.rb
+++ app/controllers/buyers_controller.rb
@@ -1,2 +1,22 @@
 class BuyersController < ApplicationController
+
+    def new 
+        @buyer = Buyer.new 
+        @item = Item.find(params[:id])
+        @seller = Seller.find_by(item_id: @item.id)
+    end
+
+    def create
+        @buyer = Buyer.create(buyer_params)
+        @seller = Seller.find_by(params[:item_id])
+        @seller.delete
+        redirect_to items_path
+    end
+
+    private
+
+    def buyer_params
+        params.require(:buyer).permit(:user_id, :item_id)
+    end
+
 end
--- app/controllers/sellers_controller.rb
+++ app/controllers/sellers_controller.rb
@@ -1,2 +1,7 @@
 class SellersController < ApplicationController
+
+    def delete
+        @seller = Seller.find_by(item_id)
+        @seller.delete 
+    end
 end
